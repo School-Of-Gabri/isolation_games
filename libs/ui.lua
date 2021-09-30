9ee1dd06c6eaf41ffdabf7e73c4154f4af5578a9
Gabri Botha <gabri.botha@kubevisor.com>
2021-09-30 08:13:40 +0200

    Move button code to its own component

--- libs/ui.lua
+++ libs/ui.lua
@@ -1,44 +1,7 @@
 local inspect = require('libs.inspect')
+local Button = require('components.Button')
 
 local ui = {}
-
--- BasicButton
-local BasicButton = {
-  height = 64
-}
-BasicButton.__index = BasicButton
-function BasicButton.new(text, fn)
-  local self = setmetatable({}, BasicButton)
-  self.text = text
-  self.fn = fn
-  return self
-end
-function BasicButton:fn()
-  print('BasicButton.fn()')
-  print('Self: ' .. inspect(self))
-end
-
--- Button
-local Button = {}
-Button.__index = Button
-function Button.new(text, fn)
-  local self = setmetatable({}, Button)
-  self.text = text
-  self.fn = fn
-  return self
-end
-setmetatable(Button, {__index = BasicButton})
-function Button:dump()
-  print('Button.dump()')
-  print(inspect(self))
-end
-
-function Button.render(x, y)
-  print('Button.render()')
-  print('Self: ' .. inspect(self))
-  print('x: ' .. inspect(x))
-  print('y: ' .. inspect(y))
-end
 
 -- ui.Button = Button
 
